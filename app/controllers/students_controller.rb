ecc3f0d3daabe4c5c84a93ddf629dabbdce56d42
Kate Bennert <kate.bennert@gmail.com>
2023-04-25 12:46:35 -0400

    commit tree 67f7fcce5872598627e23a4c586219db565da6cd

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      students = Student.where(last_name: params[:name].capitalize()).or(Student.where(first_name: params[:name].capitalize()))
+    else
+      students = Student.all
+    end
+    
     render json: students
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
